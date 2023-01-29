6967f3cc2d823d0d157168e0616a66a52a5d415a
Adalto Picotti Junior <adaltocotti.jr@gmail.com>
2023-01-28 22:11:38 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Adalto Picotti Junior on 28/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
